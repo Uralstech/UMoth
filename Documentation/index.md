486783148111b4887b81111db05ef7c736c87fc4
Udayshankar Ravikumar <udayshankar.cto@uralstech.in>
2026-06-06 07:27:52 +0530

    Updated gradle and dependencies.

--- Documentation/index.md
+++ Documentation/index.md
@@ -11,7 +11,7 @@ Google and AppleID authentication for mobile.
 
 ## Installation
 
-This package was made for Unity 6.0 and above. Built and tested in Unity 6.2.
+This package was made for Unity 6.0 and above. Built and tested in Unity 6.4.
 
 Since this package contains a native AAR plugin, it depends on the [External Dependency Manager for Unity (EDM4U)](https://github.com/googlesamples/unity-jar-resolver) to resolve native dependencies.
 You may already have EDM4U if you use Google or Firebase SDKs in your Unity project. If you do not, the installation steps are available
--- UMothAndroid/app/build.gradle.kts
+++ UMothAndroid/app/build.gradle.kts
@@ -2,17 +2,20 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+}
+
+val moduleNamespace = "com.uralstech.umoth"
+
+base {
+    archivesName.set(moduleNamespace)
 }
 
 android {
-    namespace = "com.uralstech.umoth"
-    compileSdk = 35
+    namespace = moduleNamespace
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 23
-
-        setProperty("archivesBaseName", "$namespace")
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -39,7 +42,6 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
